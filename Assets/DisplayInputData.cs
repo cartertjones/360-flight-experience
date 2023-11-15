66f3f468434b889c12cfdb8c120246143da349e7
Satii775 <124398902+Satii775@users.noreply.github.com>
2023-11-15 11:11:34 -0700

    Added Baseline Video Controls

---
+++ Assets/DisplayInputData.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using TMPro;
+
+[RequireComponent(typeof(InputData))]
+public class DisplayInputData : MonoBehaviour
+{
+    public VideoPlayerManager vpm;
+
+    private InputData _inputData;
+    private float _leftMaxScore = 0f;
+    private float _rightMaxScore = 0f;
+
+    private bool triggerValue;
+
+    private void Start()
+    {
+        _inputData = GetComponent<InputData>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (_inputData._leftController.TryGetFeatureValue(UnityEngine.XR.CommonUsages.triggerButton, out triggerValue) && triggerValue)
+        {
+            Debug.Log("Trigger button is pressed.");
+            vpm.PlayClip();
+        }
+    }
+}
---
+++ Assets/VideoPlayerManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Video;    //Must be using UnityEngine.Video
+using UnityEngine;
+
+public class VideoPlayerManager : MonoBehaviour
+{
+    public VideoPlayer videoPlayer;     //Our video player component variable. Can be set in inspector or through code in Start()             
+    public VideoClip[] clips;           //Array for video clips
+
+
+    private void Update()
+    {
+        //Check to see if the current clip has ended. Once it's over, call to PlayClip()
+        if ((videoPlayer.frame) > 0 && (videoPlayer.isPlaying == false))
+        {
+            PlayClip();
+        }
+    }
+
+    public void PlayClip()
+    {
+        if (videoPlayer.clip == clips[0])            //Checks to see if the clip that just played was the first video
+        {
+            videoPlayer.clip = clips[1];     //Sets the videoPlayer clip to the next chosen video
+        }
+        else
+        {
+            videoPlayer.clip = clips[0];     //Sets the videoPlayer clip to the next chosen video
+        }
+        
+    }
+
+   
+}
+
+
+
